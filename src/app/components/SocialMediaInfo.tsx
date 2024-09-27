b927cc3ab543abc32f42a5a5d182a9d3bbf838d0
AbbasAli <abbasali.alphasquad@gmail.com>
2024-09-27 20:49:23 +0500

    update

--- src/app/components/SocialMediaInfo.tsx
+++ src/app/components/SocialMediaInfo.tsx
@@ -48,7 +48,7 @@ const SocialMediaInfo = () => {
   return (
     <section className="w-full bg-[#f9fafb] text-[#111827] relative flex justify-center">
       <div className="w-[90%] lg:w-[80%]">
-        <div className="w-full text-center pt-40">
+        <div className="w-full text-center pt-16">
           <h1 className="text-[#111827] font-extrabold text-3xl leading-10 ">
             We’re available on more than 20+
           </h1>
@@ -76,22 +76,54 @@ const SocialMediaInfo = () => {
       <Image
         src={socialMediaInfo.vectorGroup3}
         alt="vectorGroup3"
-        className="absolute left-[117px] top-[124px] "
+        className="absolute 
+        left-[10px] top-[5px]   w-[70px]  h-[50px]
+        xs:left-[10px] xs:top-[5px]     xs:w-[80px]   xs:h-[50px]
+        sm:left-[20px]  sm:top-[5px]    sm:w-[90px] sm:h-[60px]
+        md:left-[40px]  md:top-[5px]    md:w-[100px] md:h-[70px]
+        lg:left-[40px]  lg:top-[20px]    lg:w-[120px] lg:h-[120px]
+        xl:left-[100px]  xl:top-[60px]   xl:w-[121px] xl:h-[138px]
+        2xl:left-[117px] 2xl:top-[60px] 2xl:w-[121px] 2xl:h-[138px]
+        
+    
+"
       />
       <Image
         src={socialMediaInfo.vectorGroup2}
         alt="vectorGroup2"
-        className="absolute right-[96px] top-[124px]"
+        className="absolute 
+    right-[10px] top-[10px] w-[70px] h-[50px]
+    xs:right-[20px] xs:top-[5px] xs:w-[80px] xs:h-[60px]
+    sm:right-[20px] sm:top-[5px] sm:w-[90px] sm:h-[60px]
+    md:right-[40px] md:top-[5px] md:w-[100px] md:h-[70px]
+    lg:right-[40px] lg:top-[20px] lg:w-[120px] lg:h-[120px]
+    xl:right-[117px] xl:top-[60px] xl:w-[121px] xl:h-[138px]
+    2xl:right-[90px] 2xl:top-[50px] 2xl:w-[180px] 2xl:h-[110px]
+
+       
+        
+        
+        "
       />
       <Image
         src={socialMediaInfo.vectorGroup1}
         alt="vectorGroup1"
-        className="absolute left-[112px] bottom-[54px]"
+        className="absolute 
+         left-[10px] bottom-[5px]   w-[70px]  h-[50px]
+        xs:left-[10px] xs:bottom-[5px]     xs:w-[80px]   xs:h-[50px]
+        sm:left-[20px]  sm:bottom-[5px]    sm:w-[90px] sm:h-[60px]
+        md:left-[40px]  md:bottom-[5px]    md:w-[100px] md:h-[70px]
+        lg:left-[40px]  lg:bottom-[20px]    lg:w-[120px] lg:h-[120px]
+        xl:left-[100px]  xl:bottom-[60px]   xl:w-[121px] xl:h-[138px]
+        2xl:left-[117px] 2xl:bottom-[60px] 2xl:w-[121px] 2xl:h-[138px]
+        
+        
+        "
       />
       <Image
         src={socialMediaInfo.Vector}
         alt="Vector"
-        className="absolute right-[148px] bottom-[131px]"
+        className="absolute right-[140px] bottom-[120px] h-[17px] w-[18px]"
       />
     </section>
   );
